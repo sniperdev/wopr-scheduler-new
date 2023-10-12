b9165c0f0ac3499b216026a612f3f43ffb525099
sniperdev <igorspychalaa@gmail.com>
2023-10-12 19:25:28 +0200

    <chore> remove mysql and install mysql2 for better performance

--- server/server.js
+++ server/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.use(express.json());
+app.use(express.urlencoded({
+	extended:true,
+}))
+
+
+app.get("/", (req,res)=>{
+	res.json({message: 'OK'})
+})
+
 app.listen(port, () => {
 	console.log(`Serwer działa na porcie ${port}`);
 });
