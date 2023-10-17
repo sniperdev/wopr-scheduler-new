ce21f7d3e34d38775417a54e009e207a53f083bb
sniperdev <igorspychalaa@gmail.com>
2023-10-17 18:03:03 +0200

    <feat> create login route with jwt token

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,6 +1,7 @@
 const Users = require("../models/Users");
 const Joi = require("@hapi/joi");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerSchema = Joi.object({
   name: Joi.string().min(3).required(),
@@ -45,6 +46,34 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginSchema = Joi.object({
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().required(),
+});
+
+const loginUser = async (req, res) => {
+  const { error } = loginSchema.validate(req.body);
+  if (error) return res.status(400).send(error);
+
+  const user = await Users.findOne({ where: { email: req.body.email } });
+  if (!user)
+    return res.status(400).send({
+      message: "Nie znaleziono adresu email",
+    });
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass)
+    return res.status(400).send({
+      message: "Niepoprawne hasło",
+    });
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_TOKEN);
+  return res.header("auth-token", token).send({
+    jwt: token,
+  });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,5 +4,6 @@ const auth = require("../controllers/Auth");
 const router = express.Router();
 
 router.post("/register", auth.registerUser);
+router.post("/login", auth.loginUser);
 
 module.exports = router;
