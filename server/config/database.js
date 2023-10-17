080140f0332fd4e03a87dd843409648ab395a9f8
sniperdev <igorspychalaa@gmail.com>
2023-10-17 11:55:12 +0200

    <fix> Bad port for express server

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,13 @@
-const {createConnection} = require("mysql2")
-require('dotenv').config();
-
+const { createConnection } = require("mysql2");
+require("dotenv").config();
 
 const mysqlConnection = createConnection({
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	database: process.env.DATABASE,
-	port: process.env.PORT,
-	multipleStatements: true,
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  multipleStatements: true,
+});
 
 module.exports = mysqlConnection;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv/config");
 
 const app = express();
 
@@ -13,7 +14,14 @@ app.use(
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.EXPRESS_PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}. (http://localhost:${PORT})`);
+});
+
+const mysqlConnection = require("./config/database");
+mysqlConnection.connect((err) => {
+  if (!err) {
+    console.log("Connected to database");
+  } else console.log("Connected to database ", err);
 });
