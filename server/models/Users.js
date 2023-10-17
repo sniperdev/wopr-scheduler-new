b2d28ad780a999450894ee1542d0f356028a7336
sniperdev <igorspychalaa@gmail.com>
2023-10-17 13:21:25 +0200

    <feat> create Users model

---
+++ server/models/Users.js
@@ -0,0 +1,48 @@
+const sequelize = require("../config/sequelize");
+const { DataTypes } = require("sequelize");
+
+const Users = sequelize.define("users", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  surname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  company: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    default: false,
+  },
+  createdAt: {
+    field: "created_at",
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    field: "updated_at",
+    type: DataTypes.DATE,
+  },
+});
+
+module.exports = Users;
