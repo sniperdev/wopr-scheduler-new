1f25f94c1130b16e7e5deeda9744b80b141b7fba
sniperdev <igorspychalaa@gmail.com>
2023-10-17 20:05:20 +0200

    <feat> handle login form

--- client/src/Pages/LoginPage/components/SingInFormComponent.tsx
+++ client/src/Pages/LoginPage/components/SingInFormComponent.tsx
@@ -1,18 +1,50 @@
-interface Props{
-	changeLoginForm: ()=>void;
+import React from "react";
+
+interface Props {
+  changeLoginForm: () => void;
 }
 
-const SignInFormComponent = ({changeLoginForm}:Props) => {
-	return (
-		<form className="login-page-form d-flex flex-column gap-2 w-25">
-				<input type="text" className="form-control" placeholder="Adres e-mail"/>
-				<input type="text" className="form-control" placeholder="Hasło"/>
-				<button type="submit" className="btn btn-primary">Zaloguj się</button>
-				<div className="text-center">
-					<p>Nie masz konta biznesowego? <a href="#" onClick={changeLoginForm}>Zarejestruj się</a></p>
-				</div>
-			</form>
-	);
+const SignInFormComponent = ({ changeLoginForm }: Props) => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+  };
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="login-page-form d-flex flex-column gap-2 w-25"
+      >
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Adres e-mail"
+          name="email"
+        />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Hasło"
+          name="password"
+        />
+        <button type="submit" className="btn btn-primary">
+          Zaloguj się
+        </button>
+
+        <div className="text-center">
+          <p>
+            Nie masz konta biznesowego?{" "}
+            <a href="#" onClick={changeLoginForm}>
+              Zarejestruj się
+            </a>
+          </p>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default SignInFormComponent;
