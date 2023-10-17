95498a3ae225ef4761fa18dd900225a0301c709b
sniperdev <igorspychalaa@gmail.com>
2023-10-17 11:41:13 +0200

    <feat> use cors i app

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-
-
-
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({
-	extended:true,
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(cors());
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}. (http://localhost:${PORT})`);
+  console.log(`Server is running on port ${PORT}. (http://localhost:${PORT})`);
 });
