4d046411818d5148e21de7f401dfd28ad8f370de
sniperdev <igorspychalaa@gmail.com>
2023-10-25 12:37:22 +0200

    <feat> save register information to state

--- client/src/Pages/LoginPage/components/LoginInformationComponent.tsx
+++ client/src/Pages/LoginPage/components/LoginInformationComponent.tsx
@@ -1,20 +1,58 @@
-import {Steps} from "../../../utils/interfaces/StepsInterface.ts";
+import { Steps } from "../../../utils/interfaces/StepsInterface.ts";
+import { useState } from "react";
 
 interface Props {
-	changeLoginForm: ()=>void;
+  changeLoginForm: () => void;
 }
-
-const LoginInformationComponent = ({setStep, changeLoginForm}: Steps & Props) => {
-	return (
-		<form className="login-page-form d-flex flex-column gap-2 w-25">
-			<input type="text" className="form-control" placeholder="Adres e-mail"/>
-			<input type="text" className="form-control" placeholder="Hasło"/>
-			<button type="submit" className="btn btn-primary" onClick={()=>setStep(1)}>Zarejestruj się</button>
-			<div className="text-center">
-				<p>Masz już założone konto? <a href="#" onClick={changeLoginForm}>Zaloguj się</a></p>
-			</div>
-		</form>
-	);
+interface IUser {
+  email: string;
+  password: string;
+}
+const LoginInformationComponent = ({
+  setStep,
+  changeLoginForm,
+}: Steps & Props) => {
+  const [loginInformation, setLoginInformation] = useState<IUser>({
+    email: "",
+    password: "",
+  });
+  const handleChange = (field: string, value: string) => {
+    setLoginInformation((prevLoginInfo) => ({
+      ...prevLoginInfo,
+      [field]: value,
+    }));
+  };
+  return (
+    <form className="login-page-form d-flex flex-column gap-2 w-25">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Adres e-mail"
+        onChange={(event) => handleChange("email", event.target.value)}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Hasło"
+        onChange={(event) => handleChange("password", event.target.value)}
+      />
+      <button
+        type="submit"
+        className="btn btn-primary"
+        onClick={() => setStep(1)}
+      >
+        Zarejestruj się
+      </button>
+      <div className="text-center">
+        <p>
+          Masz już założone konto?{" "}
+          <a href="#" onClick={changeLoginForm}>
+            Zaloguj się
+          </a>
+        </p>
+      </div>
+    </form>
+  );
 };
 
 export default LoginInformationComponent;
