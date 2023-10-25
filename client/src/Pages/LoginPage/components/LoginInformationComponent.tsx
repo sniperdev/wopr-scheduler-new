0eb34989bc0d4ca70afeb699ffaa2199ae8460a8
sniperdev <igorspychalaa@gmail.com>
2023-10-25 13:02:08 +0200

    <feat> add reapet password for register form

--- client/src/Pages/LoginPage/components/LoginInformationComponent.tsx
+++ client/src/Pages/LoginPage/components/LoginInformationComponent.tsx
@@ -1,13 +1,26 @@
 import { Steps } from "../../../utils/interfaces/StepsInterface.ts";
-import { useState } from "react";
+import React, { useState } from "react";
+import Joi from "joi";
 
 interface Props {
   changeLoginForm: () => void;
 }
+
 interface IUser {
   email: string;
   password: string;
+  repeatPassword: string;
 }
+
+const loginSchema = Joi.object({
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "pl"] },
+  }),
+  password: Joi.string(),
+  repeatPassword: Joi.ref("password"),
+});
+
 const LoginInformationComponent = ({
   setStep,
   changeLoginForm,
@@ -15,32 +28,53 @@ const LoginInformationComponent = ({
   const [loginInformation, setLoginInformation] = useState<IUser>({
     email: "",
     password: "",
+    repeatPassword: "",
   });
-  const handleChange = (field: string, value: string) => {
+
+  const handleInputChange = (field: string, value: string) => {
     setLoginInformation((prevLoginInfo) => ({
       ...prevLoginInfo,
       [field]: value,
     }));
   };
+
+  const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { error, value } = loginSchema.validate(loginInformation);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(value);
+      setStep(2);
+    }
+  };
+
   return (
-    <form className="login-page-form d-flex flex-column gap-2 w-25">
+    <form
+      onSubmit={(event) => handleSubmitForm(event)}
+      className="login-page-form d-flex flex-column gap-2 w-25"
+    >
       <input
         type="text"
         className="form-control"
         placeholder="Adres e-mail"
-        onChange={(event) => handleChange("email", event.target.value)}
+        onChange={(event) => handleInputChange("email", event.target.value)}
       />
       <input
         type="password"
         className="form-control"
         placeholder="Hasło"
-        onChange={(event) => handleChange("password", event.target.value)}
+        onChange={(event) => handleInputChange("password", event.target.value)}
       />
-      <button
-        type="submit"
-        className="btn btn-primary"
-        onClick={() => setStep(1)}
-      >
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Powtórz hasło"
+        onChange={(event) =>
+          handleInputChange("repeatPassword", event.target.value)
+        }
+      />
+      <button type="submit" className="btn btn-primary">
         Zarejestruj się
       </button>
       <div className="text-center">
