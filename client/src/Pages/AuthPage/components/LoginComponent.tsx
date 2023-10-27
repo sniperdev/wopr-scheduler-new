2cfd73d5b752959ef4e2c661f6aae1b72405c37a
sniperdev <igorspychalaa@gmail.com>
2023-10-27 13:52:59 +0200

    <refactor> LoginPage

--- client/src/Pages/AuthPage/components/LoginComponent.tsx
+++ client/src/Pages/AuthPage/components/LoginComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -6,78 +6,81 @@ interface Props {
   changeLoginForm: () => void;
 }
 
-interface IMutation {
+interface FormData {
   email: string;
   password: string;
 }
 
 const SignInFormComponent = ({ changeLoginForm }: Props) => {
-  const login = async (loginParameters: IMutation) => {
-    return await axios
-      .post("http://localhost:3000/login", {
-        email: loginParameters.email,
-        password: loginParameters.password,
-      })
-      .then((response) => {
-        return response.data;
-      });
-  };
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
 
-  const saveJWT = async (data: { jwt: string }) => {
-    if (data) {
-      const token: string = data.jwt;
-      localStorage.setItem("token", token);
-    }
-  };
+  async function login(loginParameters: FormData) {
+    const response = await axios.post(
+      "http://localhost:3000/login",
+      loginParameters,
+    );
+    return response.data;
+  }
+
+  async function saveJWT(data: { jwt: string }) {
+    const token: string = data.jwt;
+    localStorage.setItem("token", token);
+  }
 
   const loginMutation = useMutation({
     mutationFn: login,
-    onSuccess: async (data) => saveJWT(data),
+    onSuccess: saveJWT,
     onError: (err) => {
-      console.log("TEST" + err.message);
+      console.log(err.message);
     },
   });
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target as HTMLFormElement);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    loginMutation.mutate({ email: email, password: password });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
-  return (
-    <>
-      <form
-        onSubmit={handleSubmit}
-        className="login-page-form d-flex flex-column gap-2 w-25"
-      >
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Adres e-mail"
-          name="email"
-        />
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Hasło"
-          name="password"
-        />
-        <button type="submit" className="btn btn-primary">
-          Zaloguj się
-        </button>
 
-        <div className="text-center">
-          <p>
-            Nie masz konta biznesowego?{" "}
-            <a href="#" onClick={changeLoginForm}>
-              Zarejestruj się
-            </a>
-          </p>
-        </div>
-      </form>
-    </>
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    loginMutation.mutate(formData);
+  };
+
+  return (
+    <form
+      onSubmit={handleFormSubmit}
+      className="login-page-form d-flex flex-column gap-2 w-25"
+    >
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Adres e-mail"
+        name="email"
+        value={formData.email}
+        onChange={handleInputChange}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Hasło"
+        name="password"
+        value={formData.password}
+        onChange={handleInputChange}
+      />
+      <button type="submit" className="btn btn-primary">
+        Zaloguj się
+      </button>
+      <div className="text-center">
+        <p>
+          Nie masz konta biznesowego?{" "}
+          <a href="#" onClick={changeLoginForm}>
+            Zarejestruj się
+          </a>
+        </p>
+      </div>
+    </form>
   );
 };
 
