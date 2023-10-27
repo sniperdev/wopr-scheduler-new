988f5d3812fbb1b819fa2bbaca073854a373c800
sniperdev <igorspychalaa@gmail.com>
2023-10-27 13:09:57 +0200

    <feat> AuthPage lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import AuthPage from "./pages/AuthPage/AuthPage.tsx";
+import { lazy, Suspense } from "react";
+
+const LazyAuthPage = lazy(() => import("./pages/AuthPage/AuthPage.tsx"));
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AuthPage />}></Route>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<h1>Ładowanie...</h1>}>
+            <LazyAuthPage />
+          </Suspense>
+        }
+      ></Route>
     </Routes>
   );
 }
