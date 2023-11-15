3af638f5d7e4585509f5e5abef84a07915c4d0cc
sniperdev <igorspychalaa@gmail.com>
2023-11-15 15:13:40 +0100

    <feat> fetching user shifts data

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -8,26 +8,32 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavbarComponent from "./components/NavbarComponent.tsx";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 interface Props {
   user: User;
 }
 const HomePage = ({ user }: Props) => {
-  const fetchUserShifts = async () => {
-    const response = await fetch();
-    if (!response.ok) {
-      throw new Error("Something went wrong");
-    }
-    return response.json();
-  };
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["userShifts"],
-    queryFn: fetchUserShifts,
+  const { isPending, isError, data } = useQuery({
+    queryKey: ["userShifts", user.id],
+    queryFn: async () => {
+      const response = await axios.get(
+        "http://localhost:3000/UsersWorkShifts/" + user.id,
+        {
+          headers: {
+            "auth-token": `${localStorage.getItem("token")}`,
+          },
+        },
+      );
+      return response.data;
+    },
   });
 
   return (
     <div className="vh-100">
       <NavbarComponent user={user}></NavbarComponent>
+      {isPending && <p>Pobieranie danych kalendarza...</p>}
+      {isError && <p>Wystąpił błąd</p>}
       <div className="mx-2 mt-2 calendar">
         <FullCalendar
           plugins={[dayGridPlugin, timeGridWeek, bootstrap5Plugin]}
@@ -38,6 +44,7 @@ const HomePage = ({ user }: Props) => {
             center: "title",
             end: "timeGridWeek dayGridMonth",
           }}
+          events={data}
           locale="pl"
           weekNumberCalculation={"ISO"}
           height="100%"
